bc5a9f54ebe2724c7e8b33851f0e7c41d0de3d81
sameehkhan <>
2018-11-14 14:39:46 -0800

    Create user controller

--- 99_cats_auth/app/models/user.rb
+++ 99_cats_auth/app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ApplicationRecord
-end
+  validates :user_name, uniqueness: true, presence: true 
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password length: {minimum: 6, allow_nil = true}
+  before_validation: ensure_session_token
+  
+  attr_reader :password
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    if user && user.is_password?(password)
+      user 
+    else 
+      nil 
+    end
+  end 
+  
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end 
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password 
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end 
+  
+end 
+
